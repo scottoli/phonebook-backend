4e02bcaf85f555bf57d8edb5352b17a2b5815f13
Sco-Dot <oliver.scottd@gmail.com>
2025-07-05 00:50:37 -0400

    uibuild

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
-app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('data', (req, res) => {
